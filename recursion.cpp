a0e232f27a245b60a004c0edd7f57c4b5666c05e
DeepanshuPratik <deepanshu.pratik@gmail.com>
2022-05-21 10:45:32 +0530

    Reccursion using dp fibbo time comparison

--- recursion.cpp
+++ recursion.cpp
@@ -4,8 +4,9 @@
 //
 #include<bits/stdc++.h>
 using namespace std;
+using namespace std::chrono;
 
-int factorial(int n){
+long long factorial(long long n){
     if(n==0 || n==1)
         return 1;
     return n*factorial(n-1);
@@ -21,24 +22,50 @@ int fibbonaci(int n){
         return 1;
     return fibbonaci(n-1)+fibbonaci(n-2);
 }
-int fibbonaci_dp(int n){
-    vector<int>p(n,0);
+//
+// x^n 
+//
+int power_n(int x,int n){
+    if(n==1)
+        return x;
+    return x*power_n(x,n-1);
+}
+int fibbonaci_dp(vector<int>&p,int n){
     if(n==1)
         return 1;
     else if(n==2)
-        return 1;
-    if(p[n]==0){
-        p[n] = fibbonaci_dp(n-1)+fibbonaci_dp(n-2);
-        return p[n]; 
-    }
-    else return p[n];
+        return 1; 
+    if(p[n]!=-1)
+        return p[n];
+
+    p[n] = fibbonaci_dp(p,n-1)+fibbonaci_dp(p,n-2); 
+    
+    return p[n];
 }
 
 int main(){
-    int n;
+    long long n;
     cin>>n;
-    cout<<factorial(n)<<endl;
-    cout<<fibbonaci(n);
+    cout<<"Factorial of n: "<<factorial(n)<<endl;
+    
+    auto star = high_resolution_clock::now();
+    
+    cout<<"without dp: "<<fibbonaci(n)<<endl;
+    
+    auto sto = high_resolution_clock::now();
+    auto duratio = duration_cast<microseconds>(sto - star);
+    cout <<"Function without dp : "<<duratio.count() << endl;
+    
+    vector<int>p(n+2,-1);
+    auto start = high_resolution_clock::now();
+    
+    cout<<"with dp: "<<fibbonaci_dp(p,n)<<endl;
+    
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout <<"Function with dp : "<<duration.count() << endl;
+    
+    cout<<"Power of 2 using recurr : "<<power_n(2,n);
     return 0;
 }
 
