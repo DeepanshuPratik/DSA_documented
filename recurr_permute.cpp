8c3f37f227fd584f6b540fc35d436059d0c6cd5a
DeepanshuPratik <deepanshu.pratik@gmail.com>
2022-05-26 20:56:52 +0530

    permute string unique with repeated characters

--- recurr_permute.cpp
+++ recurr_permute.cpp
@@ -1,8 +1,22 @@
 // lexical order of permutation of a given string
-
+// time complexity: O(n*n!) space complexity: O(n)
 #include<bits/stdc++.h>
 using namespace std;
-string ans="";
+void solve_reccur_unique(string s,int pos){
+    if(pos == s.size()){
+        cout<<s<<endl;
+        return;
+    }
+    unordered_set<char> us;
+    for(int i=pos;i<s.size();i++){
+        if(us.find(s[i])!=us.end()) 
+            continue;
+        us.insert(s[i]);
+        swap(s[i],s[pos]);
+        solve_reccur_unique(s,pos+1);
+        swap(s[i],s[pos]);
+    }
+}
 void solve(string s,int pos){
     if(pos==s.size()){
         cout<<s<<endl;
@@ -18,6 +32,8 @@ int main(){
     string s;
     // cin>>s;
     // cout<<solve(s,0);
-    solve("abc",0);
+    solve("abcb",0);
+    cout<<"****************************************"<<endl;
+    solve_reccur_unique("abcb",0);
     return 0;   
 }
