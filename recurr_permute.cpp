3a51e060a8c1dd4946742c06a0fe9bb01af1fd3c
DeepanshuPratik <deepanshu.pratik@gmail.com>
2022-05-26 21:10:42 +0530

    subset added

--- recurr_permute.cpp
+++ recurr_permute.cpp
@@ -2,6 +2,20 @@
 // time complexity: O(n*n!) space complexity: O(n)
 #include<bits/stdc++.h>
 using namespace std;
+
+vector<string>a;
+
+void subset(string ans,string s,int pos){
+    if(pos == s.length()){
+        a.push_back(ans);
+        return;
+    }
+    ans.push_back(s[pos]);
+    subset(ans,s,pos+1);
+    ans.pop_back();
+    subset(ans,s,pos+1);
+    
+}
 void solve_reccur_unique(string s,int pos){
     if(pos == s.size()){
         cout<<s<<endl;
@@ -30,10 +44,14 @@ void solve(string s,int pos){
 }
 int main(){
     string s;
-    // cin>>s;
+    cin>>s;
     // cout<<solve(s,0);
     solve("abcb",0);
     cout<<"****************************************"<<endl;
     solve_reccur_unique("abcb",0);
+    cout<<"****************************************"<<endl;
+    subset("",s,0);
+    for(auto i:a)
+        cout<<i<<endl;
     return 0;   
 }
